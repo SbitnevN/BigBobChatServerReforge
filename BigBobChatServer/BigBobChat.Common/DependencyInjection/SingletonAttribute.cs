4f8b65a10c93435b09e963b6abe0df4d341a0779
Nikita <sbitnevn@yandex.ru>
2025-02-08 21:38:59 +0300

    Слои + немного DI

---
+++ BigBobChatServer/BigBobChat.Common/DependencyInjection/SingletonAttribute.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace BigBobChat.Common.DependencyInjection;
+
+public class SingletonAttribute : Attribute
+{
+    public SingletonAttribute(Type serviceType) : base(serviceType, ServiceLifetime.Singleton)
+    {
+    }
+}
---
+++ BigBobChatServer/BigBobChat.Common/DependencyInjection/TransientAttribute.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace BigBobChat.Common.DependencyInjection;
+
+public class TransientAttribute : ServiceAttribute
+{
+    public TransientAttribute(Type serviceType) : base(serviceType, ServiceLifetime.Transient)
+    {
+    }
+}
